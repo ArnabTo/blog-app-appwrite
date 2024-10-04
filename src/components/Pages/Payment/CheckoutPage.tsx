cc6b8256f45ae6966535349e857b90be315b775d
ArnabTo <arnabs929@gmail.com>
2024-10-04 18:13:29 +0600

    fixed payment issue

--- src/components/Pages/Payment/CheckoutPage.tsx
+++ src/components/Pages/Payment/CheckoutPage.tsx
@@ -5,11 +5,10 @@ import { Button } from "@nextui-org/react";
 import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const CheckoutPage = ({ secret }: { secret: string }) => {
-
     const router = useRouter();
     const stripe = useStripe();
     const elements = useElements();
@@ -17,88 +16,83 @@ const CheckoutPage = ({ secret }: { secret: string }) => {
     const [message, setMessage] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState(false);
 
-    const { user, currentUserData } = useUser();
-console.log(currentUserData)
-    const handleSubmit = async (event: any) => {
+    const { currentUserData } = useUser();
+
+    const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
 
         if (!stripe || !elements || !secret) {
+            setMessage("Stripe has not been initialized.");
             return;
         }
+
         setIsLoading(true);
+        setMessage(null);
 
-        const { error: paymentError } = await stripe.confirmPayment({
-            elements,
-            confirmParams: {
-                // return_url: 'http://localhost:3000/',
-            },
-            redirect: 'if_required',
-        })
+        try {
+            const { error: paymentError, paymentIntent } = await stripe.confirmPayment({
+                elements,
+                confirmParams: {},
+                redirect: 'if_required',
+            });
 
-        // if (paymentError.type === 'card_error' || paymentError.type === 'validation_error') {
-        //      setMessage(paymentError.message ?? 'An unexpected error occurred.');
-        // } else {
-        //     setMessage('An unexpected error occurred.');
-        // }
+            if (paymentError) {
+                throw paymentError;
+            }
 
-        if (paymentError) {
-            if (paymentError.type === 'card_error' || paymentError.type === 'validation_error') {
-                setMessage(paymentError.message ?? 'An unexpected error occurred.');
+            if (paymentIntent && paymentIntent.status === 'succeeded') {
+                if (currentUserData && currentUserData.$id) {
+                    const paymentDetails = {
+                        userId: currentUserData.$id,
+                        amount: paymentIntent.amount / 100,
+                        currency: paymentIntent.currency,
+                        paymentIntentId: paymentIntent.id,
+                        status: paymentIntent.status,
+                        paymentDate: new Date(paymentIntent.created * 1000),
+                    };
+
+                    const response = await dataBaseServices.createPaymentIntent(paymentDetails);
+                    if (response) {
+                        const plan = paymentIntent.amount === 2500 ? 'Premium' : 'Business';
+                        await dataBaseServices.updateUserPlan(currentUserData.$id, { plan: plan });
+                        toast.success('Payment successful!');
+                        setTimeout(() => {
+                            router.push(`/checkout/payment-success?amount=${paymentDetails.amount}`);
+                        }, 1000);
+                    }
+                }
+            } else {
+                setMessage("Payment status is not successful. Please try again.");
+            }
+        } catch (error: any) {
+            if (error.type === 'card_error' || error.type === 'validation_error') {
+                setMessage(error.message ?? 'An unexpected error occurred.');
             } else {
                 setMessage('An unexpected error occurred.');
             }
-        } else {
-            const { paymentIntent } = await stripe.retrievePaymentIntent(secret);
-            if (paymentIntent && paymentIntent.status === 'succeeded') {
-                console.log(paymentIntent)
-                setMessage('Payment succeeded!');
-                try {
-                    if (currentUserData && currentUserData.$id) {
-                        const paymentDetails = {
-                            userId: currentUserData.$id,
-                            amount: paymentIntent.amount / 100,
-                            currency: paymentIntent.currency,
-                            paymentIntentId: paymentIntent.id,
-                            status: paymentIntent.status,
-                            paymentDate: new Date(paymentIntent.created * 1000),
-                        }
-                        console.log(
-                            paymentDetails
-                        )
-                        const response = await dataBaseServices.createPaymentIntent(paymentDetails);
-                        if (response && currentUserData?.$id) {
-                            //update user plan on database
-                            const plan = paymentIntent.amount === 25 ? 'Premium' : 'Business';
-                            const updateUserPlan = await dataBaseServices.updateUserPlan(currentUserData.$id, { plan: plan });
-                            console.log(response, 'response in payment intent');
-                            toast.success('Payment successful!');
-                            if (updateUserPlan) {
-                                setTimeout(() => {
-                                    router.push(`/checkout/payment-success?amount=${paymentDetails.amount}`);
-                                }, 1000)
-                            }
-                        }
-                    }
-                } catch (error) {
-                    toast.error('Payment failed!');
-                    console.log(error, 'error in payment intent');
-                }
-            }
+            toast.error('Payment failed!');
+        } finally {
+            setIsLoading(false);
         }
-
-        setIsLoading(false);
-
-    }
+    };
 
     if (!secret || !stripe || !elements) {
-        return <div className="h-screen flex items-center justify-center"><Loader /></div>
+        return <div className="h-screen flex items-center justify-center"><Loader /></div>;
     }
+
     return (
         <div>
             <form id="payment-form" onSubmit={handleSubmit} className="space-y-4">
                 <PaymentElement id="payment-element" />
-                <Button className="bg-textcolor text-primary rounded-md" type="submit" disabled={!stripe}>Pay</Button>
-                {message && <p>{message}</p>}
+                <Button 
+                    className="bg-textcolor text-primary rounded-md flex items-center" 
+                    type="submit" 
+                    disabled={isLoading || !stripe}
+                >
+                    {isLoading ? <Loader className="animate-spin mr-2" strokeWidth={2} size={20}/> : null}
+                    {isLoading ? 'Processing...' : 'Pay'}
+                </Button>
+                {message && <p className="text-red-500">{message}</p>}
             </form>
         </div>
     );
